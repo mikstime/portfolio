5073c1575578b7776bdffd0b57ac586a742e0b2c
MBtsky <9250499222@mail.ru>
2019-08-07 19:44:37 +0300

    RAW pan added

--- src/Bio/Chain.js
+++ src/Bio/Chain.js
@@ -1,14 +1,19 @@
 import React, {Component} from 'react'
-import { Layer } from 'react-paper-bindings'
+import { Layer, Tool } from 'react-paper-bindings'
 import uuid from 'uuid/v4'
 import Fact from './Fact'
 
 import testData from './BioTemplate.json'
+import { View } from 'react-paper-bindings'
 class Chain extends Component {
 
     render() {
         return(
             <Layer>
+                <Tool
+                    onMouseDrag = {this.props.onDrag}
+                    onMouseUp={this.props.onUp}
+                />
                 {
                     testData
                         .map((descr, id) =>(
